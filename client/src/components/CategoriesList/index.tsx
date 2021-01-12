30433d5d5a854daa954bd9f48eac7ec105550bd0
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 18:22:00 +0200

    request categories and display list of categories

--- client/src/components/CategoriesList/index.tsx
+++ client/src/components/CategoriesList/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { SpinnerCircular } from 'spinners-react';
+
+import { GET_CATEGORIES } from '../../queries';
+
+interface CategoriesArgs {
+  categoriesArray: string[]
+}
+
+const callCategories = ({ categoriesArray }: CategoriesArgs) => {
+  if(categoriesArray.length > 0) {
+    return categoriesArray.map((category) => {
+      return <div key={category}>{category}</div>
+    })
+  }
+}
 
 const CategoriesList: React.FC = () => {
+
+  const { loading, error, data } = useQuery(GET_CATEGORIES);
+
+  if (error) {
+    return <h1>Something went wrong!</h1>;
+  }
+
+  if (loading) {
+    return <SpinnerCircular />
+  }
+
   return (
     <div>
-      Display Categories List Here
+      <h4>Categories</h4>
+      <div>
+        {callCategories({ categoriesArray: data.categories })}
+      </div>
     </div>
   );
 }
