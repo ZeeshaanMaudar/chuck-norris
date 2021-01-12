f440ceab694488810d4d82ac1891e1fc661932c8
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 19:32:18 +0200

    use passed data through useContext

--- client/src/components/RandomJoke/index.tsx
+++ client/src/components/RandomJoke/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SpinnerCircular } from 'spinners-react';
+
+import { CategoryContext } from '../Category';
 
 const RandomJoke: React.FC = () => {
+
+  const { error, loading, data } = useContext(CategoryContext);
+
+  if (error) {
+    return <h1>Something went wrong!</h1>;
+  }
+
+  if (loading) {
+    return <SpinnerCircular />
+  }
+
+  const { icon_url, value } = data.getRandomJoke;
+
   return (
     <div>
-      Random joke here
+      <img src={icon_url} alt='chuck norris avatar' />
+      <p>{value}</p>
     </div>
   );
 }
