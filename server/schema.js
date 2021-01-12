b8efa8dbf1ccd27995839c5c591c487601316eb1
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 18:57:10 +0200

    create typeDefs schema to getRandomJoke

--- server/schema.js
+++ server/schema.js
@@ -1,7 +1,13 @@
 const { gql } = require('apollo-server');
 
 exports.typeDefs = gql`
+  type RandomJoke {
+    icon_url: String
+    value: String
+  }
+
   type Query {
-    categories: [String]
+    categories: [String],
+    getRandomJoke(category: String!): RandomJoke
   }
 `;
