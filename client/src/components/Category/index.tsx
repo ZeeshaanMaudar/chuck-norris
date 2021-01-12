0ed38d2de83697794e47bd3641b2beedea1625c8
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 18:48:21 +0200

    create elelement to test accordion

--- client/src/components/Category/index.tsx
+++ client/src/components/Category/index.tsx
@@ -7,12 +7,25 @@ interface Props {
   categoryChosen: string
 }
 
+interface callRandomJokeArgs {
+  isOpen: boolean,
+  category: string,
+  categoryChosen: string
+}
+
+const callRandomJoke = ({ isOpen, category, categoryChosen }: callRandomJokeArgs) => {
+  if (isOpen && category === categoryChosen) {
+    return <div>Random Joke query</div>
+  }
+}
+
 const Category: React.FC<Props> = ({ category, handleClick, isOpen, categoryChosen }) => {
   return (
     <div>
       <div onClick={() => handleClick(category)}>
         <h4>{category}</h4>
       </div>
+      {callRandomJoke({ isOpen, category, categoryChosen })}
     </div>
   );
 }
