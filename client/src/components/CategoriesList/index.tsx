b29b52f5e515e627b2d4beba0dd5429c5e771e34
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-01-12 17:30:14 +0200

    create CategoriesList

---
+++ client/src/components/CategoriesList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoriesList: React.FC = () => {
+  return (
+    <div>
+      Display Categories List Here
+    </div>
+  );
+}
+
+export default CategoriesList;
